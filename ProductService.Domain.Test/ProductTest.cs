0297fa2b8d9b448a2a33a5d5d8fc1a22794b0004
Alvaro Julio <alvarodev@hotmail.com>
2024-02-06 17:41:50 -0500

    Adding first test. At this point the test fails.

---
+++ ProductService.Domain.Test/ProductTest.cs
@@ -0,0 +1,11 @@
+namespace ProductService.Domain.Test
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void Should_Create_A_Product_Using_Default_Ctr()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
