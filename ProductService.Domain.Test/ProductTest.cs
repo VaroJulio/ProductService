f463cb15d19ec48d30d919a765dc3887366ab370
Alvaro Julio <alvarodev@hotmail.com>
2024-02-06 20:20:59 -0500

    Adding the second test related to the product creation using parameters. Test failed

--- ProductService.Domain.Test/ProductTest.cs
+++ ProductService.Domain.Test/ProductTest.cs
@@ -17,5 +17,15 @@ namespace ProductService.Domain.Test
                 Assert.IsType<Product>(newProduct);
             });
         }
+
+        [Theory]
+        [InlineData("Cheese", 5, "Keep into the freezer", 11.58)]
+        [InlineData("T-bone steak", 8, "Pretty delicious", 30)]
+        [InlineData("Spagetti", 12, "Measured in pounds", 24)]
+        [InlineData("Bottle of Water x 500 ml", 0, "A pepsico product", 11.58)]
+        public void Should_Create_A_Product_Using_Parameters(string name, int stock, string description, decimal price)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
