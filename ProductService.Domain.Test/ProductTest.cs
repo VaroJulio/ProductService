84a59b75d86a9d63c59d8f828b8d71333640751d
Alvaro Julio <alvarodev@hotmail.com>
2024-02-06 21:31:28 -0500

    Adding the required logic to create products using parameters

--- ProductService.Domain.Test/ProductTest.cs
+++ ProductService.Domain.Test/ProductTest.cs
@@ -22,10 +22,28 @@ namespace ProductService.Domain.Test
         [InlineData("Cheese", 5, "Keep into the freezer", 11.58)]
         [InlineData("T-bone steak", 8, "Pretty delicious", 30)]
         [InlineData("Spagetti", 12, "Measured in pounds", 24)]
-        [InlineData("Bottle of Water x 500 ml", 0, "A pepsico product", 11.58)]
+        [InlineData("Bottle of Water x 500 ml", 0, "A pepsico product", 2.50)]
         public void Should_Create_A_Product_Using_Parameters(string name, int stock, string description, decimal price)
         {
-            throw new NotImplementedException();
+            Product newProduct;
+
+            newProduct = new Product(name, stock, description, price);
+
+            Assert.Multiple(() =>
+            {
+                Assert.NotNull(newProduct);
+                Assert.IsType<Product>(newProduct);
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.Equal(default, newProduct.ProductId);
+                Assert.Equal(name, newProduct.Name);
+                Assert.True(newProduct.Status);
+                Assert.Equal(stock, newProduct.Stock);
+                Assert.Equal(description, newProduct.Description);
+                Assert.Equal(price, newProduct.Price);
+            });
         }
     }
 }
--- ProductService.Domain/ProductAggregate/Product.cs
+++ ProductService.Domain/ProductAggregate/Product.cs
@@ -2,6 +2,26 @@
 {
     public class Product
     {
-        public Product() { }
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public Product() 
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+        }
+
+        public Product(string name, int stock, string description, decimal price) 
+        {
+            Name = name;
+            Stock = stock;
+            Description = description;
+            Price = price;
+            Status = true;
+        }
     }
 }
