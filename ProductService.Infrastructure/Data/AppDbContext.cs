da788b9b591822320e7908c3bdad6505f47492fe
Alvaro Julio <alvarodev@hotmail.com>
2024-02-11 14:24:25 -0500

    Adding the db context

---
+++ ProductService.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Domain.ProductAggregate;
+using System.Reflection;
+
+namespace ProductService.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            var result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            return result;
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ ProductService.Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductService.Domain.ProductAggregate;
+
+namespace ProductService.Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(p => p.ProductId);
+            builder.Property(p => p.ProductId).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Status).IsRequired().HasDefaultValue(true);
+            builder.Property(p => p.Stock).IsRequired().HasDefaultValue(default);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(500);
+            builder.Property(p => p.Price).IsRequired().HasPrecision(18,2).HasDefaultValue(default);
+            builder.HasQueryFilter(p => p.Status.Equals(true));
+        }
+    }
+}
