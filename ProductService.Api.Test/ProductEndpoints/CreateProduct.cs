f2c424b8e95ca2052f74bd6e7489ae75a848cf9f
Alvaro Julio <alvarodev@hotmail.com>
2024-02-12 16:03:41 -0500

    Adding test for create product endpoint. At this point the test fails.

---
+++ ProductService.Api.Test/ProductEndpoints/CreateProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductService.Api.Test.ProductEndpoints
+{
+    public class CreateProduct
+    {
+        [Fact]
+        public async Task Should_Create_Product()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
