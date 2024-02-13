4f13e268032beb938ba22660904c1135fa1f651c
Alvaro Julio <alvarodev@hotmail.com>
2024-02-13 10:43:22 -0500

    Adding test for Get product By Id. At this moment the test fail.

---
+++ ProductService.Api.Test/ProductEndpoints/GetByIdProductTest.cs
@@ -0,0 +1,13 @@
+﻿using AutoFixture.Xunit2;
+
+namespace ProductService.Api.Test.ProductEndpoints
+{
+    public class GetByIdProductTest
+    {
+        [Theory, AutoData]
+        public async Task Should_Get_Product_By_Id()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
