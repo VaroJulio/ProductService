8584662e648a8c9ff33d609f36329e0d8ce084a8
Alvaro Julio <alvarodev@hotmail.com>
2024-02-13 12:42:55 -0500

    Adding test for Update product endpoint. At this point the test fails.

---
+++ ProductService.Api.Test/ProductEndpoints/UpdateProductTest.cs
@@ -0,0 +1,13 @@
+﻿using AutoFixture.Xunit2;
+
+namespace ProductService.Api.Test.ProductEndpoints
+{
+    public class UpdateProductTest
+    {
+        [Theory, AutoData]
+        public async Task Should_Update_Product()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
